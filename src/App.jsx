40dc2c37798bbabad97561bbb483e02738b6d1f4
I.M.S.P.Herath. <100229009+saumyapiumi7@users.noreply.github.com>
2025-05-09 08:39:11 +0530

    add file and setup app

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+
+
+function App() {
+  return (
+    <>
+      <div className="screen">
+        
+
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
